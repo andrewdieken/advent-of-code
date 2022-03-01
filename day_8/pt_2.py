bd56a7c9042d975a8c1da373b7b24d14bfc07685
adieken <adieken@guidebook.com>
2022-02-28 21:16:29 -0800

    Add changes

--- day_8/pt_2.py
+++ day_8/pt_2.py
@@ -1,3 +1,45 @@
+def generate_known_segment_connection_mappings(wire_segment_connections):
+    """
+    """
+    segment_count_to_digit_mappings =  {
+        2: 1,
+        4: 4,
+        3: 7,
+        7: 8
+    }
+
+    known_mappings = {
+        1: "",
+        4: "",
+        7: "",
+        8: ""
+    }
+
+    for segment_combo in wire_segment_connections.split(' '):
+        combo_len = len(segment_combo)
+        if segment_count_to_digit_mappings.get(combo_len):
+            known_mappings[segment_count_to_digit_mappings.get(combo_len)] = segment_combo
+
+    return known_mappings
+
+def generate_segment_postioning(known_segment_connection_mappings):
+    """
+    Generate postioning of segments based off the known segment connection
+    mappings.
+    """
+    segment_positioning = {
+        "T": "",
+        "TL": "",
+        "TR": "",
+        "M": "",
+        "B": "",
+        "BL": "",
+        "BR": ""
+    }
+
+    # T -> difference between 1 & 7
+    segment_positioning["T"] = set(known_segment_connection_mappings[7]) - set(known_segment_connection_mappings[1])
+
 def generate_wire_segment_connection_mappings(wire_segment_connections):
     """
     """
@@ -14,37 +56,29 @@ def generate_wire_segment_connection_mappings(wire_segment_connections):
         9: ""
     }
 
-    segment_count_to_digit_mappings =  {
-        2: 1,
-        4: 4,
-        3: 7,
-        7: 8
-    }
+    # Get known mappings
+    known_segment_connection_mappings = generate_known_segment_connection_mappings(wire_segment_connections)
+    mappings.update(known_segment_connection_mappings)
+
+    # Determine segment positioning
+
+    # Get unknown mappings based off segment positioning
 
     # Get known segment combos (1, 4, 7, 8)
     for segment_combo in wire_segment_connections.split(' '):
+        import ipdb; ipdb.set_trace()
         combo_len = len(segment_combo)
         if segment_count_to_digit_mappings.get(combo_len) and not mappings[segment_count_to_digit_mappings.get(combo_len)]:
             mappings[segment_count_to_digit_mappings.get(combo_len)] = segment_combo
 
     return mappings
 
-def generate_list_of_output_segment_combos(raw_input):
-    """
-    """
-    segment_combos = set()
-    for input_value in raw_input:
-        _, segment_combo_outputs = input_value.strip().split(' | ')
-        segment_combos.update(segment_combo_outputs.split(' '))
-
-    return list(segment_combos)
-
 def decode_and_add_output_values(entry_output, segment_mappings):
     """
     """
     output_sum = 0
     for output in entry_output.split(' '):
-        output_sum += segment_mappings[ouput]
+        output_sum += segment_mappings[output]
 
 # =========
 # Read input
@@ -55,7 +89,7 @@ with open(FILE_NAME) as file:
 
 answer = 0
 for entry in raw_input:
-    wire_segment_connections, output_values = input_value.strip().split(' | ')
+    wire_segment_connections, output_values = entry.strip().split(' | ')
     wire_segment_connection_mappings = generate_wire_segment_connection_mappings(wire_segment_connections)
     output_value_sum = decode_and_add_output_values(output_values, wire_segment_connection_mappings)
     answer += output_value_sum
