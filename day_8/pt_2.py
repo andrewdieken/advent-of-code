a40bb9024a2812de197f3765bfa157868c8ff14f
andrewdieken <andrewrd@live.com>
2022-03-01 21:21:24 -0800

    Final day 8 solution

--- day_8/pt_2.py
+++ day_8/pt_2.py
@@ -15,14 +15,14 @@ def generate_known_segment_connection_mappings(wire_segment_connections):
         8: ""
     }
 
-    for segment_combo in wire_segment_connections.split(' '):
+    for segment_combo in wire_segment_connections:
         combo_len = len(segment_combo)
         if segment_count_to_digit_mappings.get(combo_len):
-            known_mappings[segment_count_to_digit_mappings.get(combo_len)] = segment_combo
+            known_mappings[segment_count_to_digit_mappings.get(combo_len)] = ''.join(sorted(segment_combo))
 
     return known_mappings
 
-def generate_wire_segment_connection_mappings(wire_segment_connections):
+def generate_wire_segment_connection_mappings(signal_patterns):
     """
     """
     mappings = {
@@ -39,37 +39,52 @@ def generate_wire_segment_connection_mappings(wire_segment_connections):
     }
 
     # Get known mappings
-    known_segment_connection_mappings = generate_known_segment_connection_mappings(wire_segment_connections)
+    known_segment_connection_mappings = generate_known_segment_connection_mappings(signal_patterns)
     mappings.update(known_segment_connection_mappings)
 
     #
     # Get 6 segment length digits
     #
-
-    # 0
-
-    # 9
-
-    # 6
+    six_segment_length_patterns = [pattern for pattern in signal_patterns if len(pattern) == 6]
+    for pattern in six_segment_length_patterns:
+        # 9
+        if all(segment in pattern for segment in mappings[4]):
+            mappings[9] = ''.join(sorted(pattern))
+        # 0
+        elif all(segment in pattern for segment in mappings[1]):
+            mappings[0] = ''.join(sorted(pattern))
+        # 6
+        else:
+            mappings[6] = ''.join(sorted(pattern))
 
     #
     # Get 5 segment length digits
     #
-
-    # 3
-
-    # 5
-
-    # 2
+    five_segment_length_patterns = [pattern for pattern in signal_patterns if len(pattern) == 5]
+    for pattern in five_segment_length_patterns:
+        # 3
+        if all(segment in pattern for segment in mappings[1]):
+            mappings[3] = ''.join(sorted(pattern))
+        # 5
+        elif all(segment in mappings[6] for segment in pattern):
+            mappings[5] = ''.join(sorted(pattern))
+        # 2
+        else:
+           mappings[2] = ''.join(sorted(pattern))
 
     return mappings
 
 def decode_and_add_output_values(entry_output, segment_mappings):
     """
     """
-    output_sum = 0
-    for output in entry_output.split(' '):
-        output_sum += segment_mappings[output]
+    reverse_mappings = {v:k for k,v in segment_mappings.items()}
+
+    output_num = ''
+    for signal_pattern in entry_output:
+        sorted_output = ''.join(sorted(signal_pattern))
+        output_num += str(reverse_mappings[sorted_output])
+
+    return int(output_num)
 
 # =========
 # Read input
@@ -80,9 +95,9 @@ with open(FILE_NAME) as file:
 
 answer = 0
 for entry in raw_input:
-    wire_segment_connections, output_values = entry.strip().split(' | ')
-    wire_segment_connection_mappings = generate_wire_segment_connection_mappings(wire_segment_connections)
-    output_value_sum = decode_and_add_output_values(output_values, wire_segment_connection_mappings)
+    signal_patterns, output_values = entry.strip().split(' | ')
+    wire_segment_connection_mappings = generate_wire_segment_connection_mappings(signal_patterns.split(' '))
+    output_value_sum = decode_and_add_output_values(output_values.split(' '), wire_segment_connection_mappings)
     answer += output_value_sum
 
 print(answer)
